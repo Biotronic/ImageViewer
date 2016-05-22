0c72de14a9fabe96ecc122a1e0b057fa39a578f1
biotronic <simen.kjaras@gmail.com>
2016-05-22 22:15:10 +0000

    Cleaned up minor code issues

--- Image Viewer/ExtensionMethods.cs
+++ Image Viewer/ExtensionMethods.cs
@@ -1,12 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
-using System.Drawing.Imaging;
-using System.IO;
 using System.Linq;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Media.Imaging;
 
 namespace ImageViewer
 {
